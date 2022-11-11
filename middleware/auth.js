2efff2c52c5e9f655a6c743f15e9c7832a71b63d
ALEXANDER ENRIQUE TOSCANO RICARDO <atoscano@correo.unicordoba.edu.co>
2022-11-11 10:18:35 -0500

    Add logs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 const auth = async (req, res, next) => {
-  console.log("Data", req.params);
+  // Se valida el parametro enviado, pero se puede validar cualquier otro factor de autenticacion
   if (req.params.id === "123") {
+    // si se permite acceder a la ruta, se llama a next() para continuar con la funcion de la ruta
     next();
   } else {
+    // si no se permite acceder a la ruta, se envia un mensaje de error
     res.send("You are not authorized");
   }
 };
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// modulo de autenticacion
 const auth = require("./middleware/auth");
 
 // paths
+
+// ruta publica
 app.get("/", (req, res) => res.send("Hello World!"));
 
+// Ruta privada
 app.get("/admin/:id", auth, (req, res) => res.send("Hello Admin!"));
+// Se agrega la funcion ^ auth ^ como segundo parametro de la ruta
 
 // server
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
